c7aef8d682de5552857fe9b61a7ed738713c921f
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2025-05-21 19:19:44 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM apache/kafka:latest
+
+
+#kafka-ui
+WORKDIR /tmp
+
+ENV ENABLE_KAFKA_UI=true
+ENV AUTH_TYPE="LOGIN_FORM"
+ENV SPRING_SECURITY_USER_NAME=admin
+ENV SPRING_SECURITY_USER_PASSWORD=passw0rd
+
+ENV KAFKA_UI_JAR=kafka-ui-api-v0.7.2.jar
+RUN wget https://github.com/provectus/kafka-ui/releases/download/v0.7.2/${KAFKA_UI_JAR}
+COPY kafka-ui.yaml /tmp/kafka-ui.yaml
+
+EXPOSE 8080
+
+#kafka
+WORKDIR /
+
+EXPOSE 9092
+
+COPY start-kafka.sh /etc/kafka/docker/
+
+
+USER appuser
+#VOLUME ["/etc/kafka/secrets", "/var/lib/kafka/data", "/mnt/shared/config"]
+
+CMD ["bash","/etc/kafka/docker/start-kafka.sh"]
---
+++ start-kafka.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#start kafka UI
+if [ "$ENABLE_KAFKA_UI" == "true" ]; then
+  java -Dspring.config.additional-location=/tmp/kafka-ui.yaml --add-opens java.rmi/javax.rmi.ssl=ALL-UNNAMED -jar /tmp/${KAFKA_UI_JAR} &
+fi
+
+#create server.properties
+cat <<EOF > /mnt/shared/config/server.properties
+node.id=1
+listeners=CONTROLLER://:29093,CLIENT://:9092,BROKER://:19092,UI://:39092
+advertised.listeners=CLIENT://${HOSTNAME}:9092,UI://localhost:39092,BROKER://localhost:19092
+process.roles=broker,controller
+controller.quorum.voters=1@localhost:29093
+inter.broker.listener.name=BROKER
+controller.listener.names=CONTROLLER
+cluster.id=4Q6g6nSWT-zMDtK--x86sw
+offsets.topic.replication.factor=1
+group.initial.rebalance.delay.ms=0
+transaction.state.log.min.isr=1
+transaction.state.log.replication.factor=1
+share.coordinator.state.topic.replication.factor=1
+sha re.coordinator.state.topic.min.isr=1
+log.dirs=/tmp/kraft-combined-logs
+log.retention.hours=0
+log.retention.minutes=2
+log.retention.check.interval.ms=20000
+listener.security.protocol.map=CONTROLLER:PLAINTEXT,BROKER:PLAINTEXT,CLIENT:PLAINTEXT,UI:PLAINTEXT
+EOF
+
+
+
+exec /etc/kafka/docker/run
+
